c5dc8ef6aa6cf866a7cb7c00fbbc48b12bfe687d
assafsauer <as.sauer@gmail.com>
2021-09-15 09:58:24 +0200

    Create s3.tf

---
+++ s3.tf
@@ -0,0 +1,13 @@
+resource "aws_s3_bucket" "prod_bucket" {
+    bucket = "tamproject2"
+    acl = "public-read"
+
+    cors_rule {
+        allowed_headers = ["*"]
+        allowed_methods = ["PUT","POST"]
+        allowed_origins = ["*"]
+        expose_headers = ["ETag"]
+        max_age_seconds = 3000
+    }
+
+}
